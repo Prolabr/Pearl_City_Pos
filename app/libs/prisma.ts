7e4d436663ef1db6910b504743c85d693f565dfc
Heshi99 <prabhashanimaheshika@gmail.com>
2025-10-31 22:09:32 +0530

    schema fix

--- app/libs/prisma.ts
+++ app/libs/prisma.ts
@@ -1,11 +1,15 @@
+import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
 export const prisma =
-  globalForPrisma.prisma ||
+  global.prisma ||
   new PrismaClient({
     log: ["query", "error", "warn"],
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
